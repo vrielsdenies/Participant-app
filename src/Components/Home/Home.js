6f8cc0b8511dbf7a94e03e0b1b5e0cbf515f381f
Niels de Vries <info@nielsdevries.net>
2018-09-08 15:28:08 +1000

    added filter functionality to search for users in pipeline

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -7,20 +7,32 @@ import { db } from "../../Firebase";
 
 import "./Home.css";
 
+//function below filters user list based on username & searchTerm
+const isSearched = searchTerm => item =>
+  item.username.toLowerCase().includes(searchTerm.toLowerCase());
+
 class HomePage extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
       users: null,
-      phases: ["Nieuw", "Wachten", "Nog niet actief", "Actief", "Klaar"]
+      phases: ["Nieuw", "Wachten", "Nog niet actief", "Actief", "Klaar"],
+      searchTerm: ""
     };
+    this.onSearchChange = this.onSearchChange.bind(this);
   }
 
   componentDidMount() {
     db.onceGetUsers().then(snapshot =>
       this.setState({ users: snapshot.val() })
     );
+  }
+
+  onSearchChange(event) {
+    this.setState({
+      searchTerm: event.target.value
+    });
   }
 
   render() {
@@ -31,7 +43,15 @@ class HomePage extends Component {
       <div>
         <h1>Home</h1>
         <p>The Home Page is accessible by every signed in user.</p>
-
+        <div className="Filter">
+          <form>
+            <input
+              type="text"
+              onChange={this.onSearchChange}
+              placeholder="Search for user"
+            />
+          </form>
+        </div>
         <div className="Pipeline-Columns">
           {this.state.phases.map(phaseName => {
             return (
@@ -42,22 +62,12 @@ class HomePage extends Component {
                   </Typography>
                 </div>
 
-                {/* <ParticipantList
-                  users={this.props.users.filter(
-                    user => user.phase === phaseName
-                  )}
-                  phaseChance={this.props.phaseChance}
-                  phases={this.props.phases}
-                /> */}
-
                 {!!users && (
                   <UserList
-                    users={Object.values(users).filter(
-                      user => user.phase === phaseName
-                    )}
-                    // users={this.props.users.filter(
-                    //   user => user.phase === phaseName
-                    // )}
+                    //Object.values makes an array from the users object. List gets filtered by phasename and searchterm
+                    users={Object.values(users)
+                      .filter(isSearched(this.state.searchTerm))
+                      .filter(user => user.phase === phaseName)}
                     phases={this.state.phases}
                     phaseChance={this.props.phaseChance}
                     phaseName={phaseName}
@@ -67,14 +77,6 @@ class HomePage extends Component {
             );
           })}
         </div>
-
-        {/* {!!users && (
-          <UserList
-            users={users}
-            phases={this.props.phases}
-            phaseChance={this.props.phaseChance}
-          />
-        )} */}
       </div>
     );
   }
